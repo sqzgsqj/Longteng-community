531c63c540e8377f7f1db218c8c3b63692e3606f
lizhiyuan <148334733@qq.com>
2017-09-29 16:27:31 +0800

    完成发表问题

--- model/Question.js
+++ model/Question.js
@@ -3,9 +3,74 @@
  */
 //问题表
 const mongoose = require('mongoose');
+const shortid = require('shortid');
 const Schema = mongoose.Schema;
 const QuestionSchema = new Schema({
-
+    _id:{
+        type:String,
+        default:shortid.generate,
+        unique:true
+    },
+    title:{
+        type:String,
+        require:true
+    },
+    content:{
+        type:String,
+        require:true
+    },
+    category:{
+        type:String,
+        require:true
+    },
+    create_time:{
+        type:Date,
+        default:Date.now
+    },
+    update_time:{
+        type:Date,
+        default:Date.now
+    },
+    tags:[String],
+    //点击量
+    click_num:{
+        type:Number,
+        default:0
+    },
+    //回复量
+    comment_num:{
+        type:Number,
+        default:0
+    },
+    //关注量
+    follow_num:{
+        type:Number,
+        default:0
+    },
+    //作者
+    author:{
+        type:String,
+        ref:'User'
+    },
+    //最后回复的帖子
+    last_reply:{
+        type:String,
+        ref:'Reply'
+    },
+    last_reply_time:{
+        type:Date,
+        default:Date.now
+    },
+    //最后回复的人
+    last_reply_author:{
+        type:String,
+        ref:'User'
+    },
+    //是否被删除
+    deleted:{
+        type:Boolean,
+        default:false
+    }
 })
 const Question = mongoose.model('Question',QuestionSchema);
 module.exports = Question
--- routes/question.js
+++ routes/question.js
@@ -2,6 +2,11 @@
  * Created by hama on 2017/9/18.
  */
 const setting = require('../setting');
+const validator = require('validator');
+//引入问题表
+const Question = require('../model/Question');
+//引入用户表
+const User = require('../model/User');
 //新建问题的处理函数
 exports.create = (req,res,next)=>{
     res.render('create-question',{
@@ -12,6 +17,44 @@ exports.create = (req,res,next)=>{
 }
 //新建行为的处理函数
 exports.postCreate = (req,res,next)=>{
+    let title = validator.trim(req.body.title);
+    let category = validator.trim(req.body.category);
+    let content = validator.trim(req.body.content);
+    let error;
+    if(!validator.isLength(title,{min:8,max:20})){
+        error = '标题长度不合法,请重新输入';
+    }
+    if(!validator.isLength(content,{min:10})){
+        error = '问题内容长度不合法,请重新输入';
+    }
+    if(error){
+        return res.end(error);
+    }else{
+        //验证成功后
+        req.body.author = req.session.user._id;
+        let newQuestion = new Question(req.body);
+        console.log(newQuestion);
+        newQuestion.save().then(question=>{
+            //某个人发布一篇文章，积分+5,发布数量+1
+            User.getUserById(req.session.user._id,(err,user)=>{
+                if(err){
+                    return res.end(err);
+                }
+                user.score += 1;
+                user.article_count += 1;
+                user.save();
+                req.session.user = user;
+                //返回的是一个添加问题的页面地址。
+                res.json({url:`/question/${question._id}`})
+            })
+            //发送at消息
+            /*at.sendMessageToMentionUsers(content,article._id,req.session.user._id);*/
+        }).catch(err=>{
+            return res.end(err);
+        })
+    }
+
+
 
 }
 //编辑问题的处理函数
@@ -28,5 +71,8 @@ exports.delete = (req,res,next)=>{
 }
 //查询问题的处理函数
 exports.index = (req,res,next)=>{
-
+    res.render('question',{
+        title:'问题详情',
+        layout:'indexTemplate'
+    })
 }
