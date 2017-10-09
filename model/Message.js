16965d51c9c921950d13c32b7bac001113ba27bf
lizhiyuan <148334733@qq.com>
2017-10-09 16:54:10 +0800

    显示已读消息和未读消息

--- model/Message.js
+++ model/Message.js
@@ -45,8 +45,17 @@ const MessageSchema = new Schema({
     }
 })
 MessageSchema.statics = {
+    //获取消息的数量
     getMessagesCount:(id,callback)=>{
         Message.count({'target_id':id,'has_read':false},callback)
+    },
+    //读取未读消息
+    getUnReadMessages:(id,callback)=>{
+        Message.find({'target_id':id,'has_read':false},null,{sort:'-create_time'}).populate('author_id').populate('target_id').populate('question_id').exec(callback);
+    },
+    //读取已读消息
+    getReadMessages:(id,callback)=>{
+        Message.find({'target_id':id,'has_read':true},null,{sort:'-create_time',limit:20}).populate('author_id').populate('target_id').populate('question_id').exec(callback);
     }
 }
 const Message = mongoose.model('Message',MessageSchema);
--- routes/message.js
+++ routes/message.js
@@ -2,10 +2,39 @@
  * Created by hama on 2017/9/18.
  */
 //消息列表的处理函数
+const Message = require('../model/Message');
 exports.index = (req,res,next)=>{
-    res.render('message-list',{
-        title:'消息列表',
-        layout:'indexTemplate'
+    //未读消息和已读消息两种消息分别查询出来，放到这个页面里面去
+    //读取的是当前登录用户的已读消息和未读消息
+    let mission1 = new Promise((resolve,reject)=>{
+        Message.getUnReadMessages(req.session.user._id,(err,undatalist)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(undatalist);
+            }
+        })
+    })
+    let mission2 = new Promise((resolve,reject)=>{
+        Message.getReadMessages(req.session.user._id,(err,datalist)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve(datalist);
+            }
+        })
+    })
+    Promise.all([mission1,mission2]).then((result)=>{
+        let read = result[1];
+        let no_read = result[0];
+        res.render('message-list',{
+            title:'消息列表',
+            layout:'indexTemplate',
+            no_read:no_read,
+            read:read
+        })
+    }).catch(err=>{
+        console.log(err);
     })
 }
 //更新某个消息的处理函数
